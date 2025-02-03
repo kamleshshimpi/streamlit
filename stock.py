916dc9416d88cf18d62d4128ca6e64e846244f67
kamlesh Shimpi <53539237+kamleshshimpi@users.noreply.github.com>
2025-02-03 07:56:12 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -26,16 +26,18 @@ ticker_sidebar_sel_itm = str(ticker_sidebar_sel_itm)
 
 raw_data = yf.download(tickers= ticker_sidebar_sel_itm, start='2020-01-01' )
 
+df1 = raw_data.copy()
+
+df1 = df1.sort_index(ascending=False)
+
+st.write("Historical Raw Data for stocks is as follows:")
+st.write(df1)
+
 df = raw_data.copy()
 
 df = df.asfreq('b')
 
 df = df.fillna(method = 'bfill')
-
-df = df.sort_index(ascending=False)
-
-st.write("Historical Raw Data for stocks is as follows:")
-st.write(df)
 
 st.write("The Line Chart for the stock is as follows:")
 st.line_chart(df.Close)
